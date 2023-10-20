7976424aca0af1db7e11b24c3f74df4c88551e1c
tjeremiahhh <axel.max97@gmail.com>
2023-10-20 15:32:00 +0800

    Improve : cors

--- src/main/java/com/security/authenticationservice/authentication/AuthenticationController.java
+++ src/main/java/com/security/authenticationservice/authentication/AuthenticationController.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/authentication")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
